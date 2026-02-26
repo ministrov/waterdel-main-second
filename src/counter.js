ef1feb13b991f15f171291721a03a297460dfc20
Zhila <ministrov2018@gmail.com>
2026-02-26 13:44:06 +0300

    feat: delete all scafolding code from main js

--- src/counter.js
+++ src/counter.js
@@ -1,9 +1,15 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+export const one = 1;
+
+// export function setupCounter(element) {
+//   let counter = 0;
+
+//   console.log(counter);
+//   const setCounter = (count) => {
+//     counter = count;
+
+//     console.log(count);
+//     element.innerHTML = `count is ${counter}`;
+//   };
+//   element.addEventListener('click', () => setCounter(counter + 1));
+//   setCounter(0);
+// }
