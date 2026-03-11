cd2e99021c798a3061655a20c3a755c1fea08559
Zhila <ministrov2018@gmail.com>
2026-03-11 11:26:32 +0300

    feat: add realisation in main js to close mobile menu when clicking outside

--- src/main.js
+++ src/main.js
@@ -11,7 +11,23 @@ jQuery(document).ready(function ($) {
     $('.header__mobile-menu').removeClass('active');
   }
 
+  // Close mobile menu when clicking close button
   $('.mobile-menu__close').on('click', function () {
     closeMobileMenu();
+  });
+
+  // Close mobile menu when clicking on links inside
+  $('.header__mobile-menu a').on('click', function () {
+    closeMobileMenu();
+  });
+
+  // Close mobile menu when clicking outside
+  $(document).on('click', function (e) {
+    if (
+      !$(e.target).closest('.header__mobile-menu, #mobile-menu-burger').length
+    ) {
+      closeMobileMenu();
+      console.log('click!!!!');
+    }
   });
 });
