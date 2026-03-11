a3a5cb8f663de71a9ec4b6f6735834e2ec2e4ff4
Zhila <ministrov2018@gmail.com>
2026-03-11 10:56:58 +0300

    feat: create a realisation with jquery library to open and close burger menu

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-const question = 'question';
+jQuery(document).ready(function ($) {
+  // Mobile menu toggle
+  $('#mobile-menu-burger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.header__mobile-menu').toggleClass('active');
+  });
 
-console.log(question);
+  // Close mobile menu function
+  function closeMobileMenu() {
+    $('#mobile-menu-burger').removeClass('active');
+    $('.header__mobile-menu').removeClass('active');
+  }
+
+  $('.mobile-menu__close').on('click', function () {
+    closeMobileMenu();
+  });
+});
